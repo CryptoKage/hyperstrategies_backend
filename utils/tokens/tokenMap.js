d299024e2a955c47e9c74952afeb8ef839220794
CryptoKage <chris_aze@hotmail.com>
2025-07-03 16:58:39 +0100

    fix: withdrawls

--- utils/tokens/tokenMap.js
+++ utils/tokens/tokenMap.js
@@ -1,16 +1,20 @@
-// utils/tokenMap.js
+// utils/tokens/tokenMap.js
+
+const usdcAbi = require('./usdcAbi.json');
 
 const tokenMap = {
   usdc: {
     symbol: 'USDC',
     address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     decimals: 6,
+    abi: usdcAbi
   },
   usdt: {
     symbol: 'USDT',
     address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
     decimals: 6,
-  },
+    abi: usdcAbi // temporarily reuse until USDT ABI added
+  }
 };
 
 module.exports = tokenMap;
