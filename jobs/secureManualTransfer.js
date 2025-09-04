7f7160194915313a21c5618ff4717eebfe82ad7e
CryptoKage <chris_aze@hotmail.com>
2025-09-04 02:31:39 +0100

    upgrade 0.9

--- jobs/secureManualTransfer.js
+++ jobs/secureManualTransfer.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 // Minimal ABI with just the 'transfer' function and 'decimals'
 const MINIMAL_ERC20_ABI = [
     {"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},
-    {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint2c56"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
+    {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
 ];
 
 const runTransfer = async () => {
