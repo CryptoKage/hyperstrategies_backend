beeecc71a4db392c3cbdf7a889497d4a516bf6af
CryptoKage <chris_aze@hotmail.com>
2025-10-18 05:21:23 +0100

    upgrade 0.9.9

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ require('./passport-setup');
 // --- Job & Utility Imports ---
 const { updateVaultPerformance } = require('./jobs/updateVaultPerformance');
 const { verifyWithdrawalSweeps } = require('./jobs/verifyWithdrawalSweeps');
-const { initializeWebSocketProvider } = require('./utils/alchemyWebsocketProvider'); 
+//const { initializeWebSocketProvider } = require('./utils/alchemyWebsocketProvider'); 
 const { subscribeToNewBlocks } = require('./jobs/pollDeposits'); 
 const cron = require('node-cron');
 
